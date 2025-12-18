a8c4f980e5f4e304936882f2cc34f52a3819b9a7
gal <newmangal@msn.com>
2025-12-18 17:21:53 +0200

    add content to the functions on both functions and logic

--- functions.py
+++ functions.py
@@ -1,17 +1,25 @@
+import random
+import logic
+from guess_game.messages import high_guess, low_guess, win
+
+
 def get_num():
-    ...
+    my_number = random.randrange(1,100)
+    return my_number
+
+def is_high(my_number:int, valid_num:int):
+    if my_number > valid_num:
+        return high_guess
 
 
-def is_high():
-    ...
+def is_low(my_number:int, valid_num:int):
+    if my_number < valid_num:
+        return low_guess
 
 
-def is_low():
-    ...
-
-
-def is_num():
-    ...
+def is_num(my_number:int, valid_num:int):
+    if my_number == valid_num:
+        return win
 
 
 if __name__ == "__main__":
--- logic.py
+++ logic.py
@@ -1,14 +1,21 @@
+
+
+
 def player_input():
-    ...
+    num = input("Please enter a number between 1-100")
+    return num
 
 
-def is_valid():
-    ...
+def is_valid(num):
+    if isinstance(num, int):
+        if not num < 0 and not num > 100:
+            valid_num = num
+        return valid_num
 
 
-def turn():
-    ...
+
 
 
 if __name__ == "__main__":
     ...
+
