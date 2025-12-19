2bc9b36145e2f38dca05a38853001f0cde0b0751
gal <newmangal@msn.com>
2025-12-19 15:30:44 +0200

    fixed main

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from guess_game.logic import player_input, is_valid
 from guess_game.messages import start_message, try_again
 from guess_game.functions import get_num, is_high, is_low ,is_num
 
-mynum = get_num()
-player_num = player_input()
 
-def turn(my_num,player_num):
+def turn(my_num, player_num):
     valid = is_valid(player_num)
     while valid != try_again:
         if is_high(mynum, valid):
@@ -26,11 +24,12 @@ def turn(my_num,player_num):
 
 
 if __name__ == "__main__":
+    mynum = get_num()
     print(start_message)
 
     while True:
-        player_num
+        player_num = player_input()
         if turn(mynum,player_num):
-            continue
+            break
 
 
