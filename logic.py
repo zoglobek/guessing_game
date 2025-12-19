ade79b524708b308aa14c27441d713403c4775c1
gal <newmangal@msn.com>
2025-12-19 15:11:14 +0200

    fixed some logic still struggling on main

--- logic.py
+++ logic.py
@@ -2,15 +2,17 @@ from guess_game.messages import try_again
 
 
 def player_input():
-    num = input("Please enter a number between 1-100")
+    num = int(input("Please enter a number between 1-100\n"))
     return num
 
 
 def is_valid(num):
     if isinstance(num, int):
-        if not num < 0 and not num > 100:
+        while  num >= 0 and num <= 100:
             valid_num = num
-        return valid_num
+            return valid_num
+        else:
+            return try_again
     else:
         return try_again
 
@@ -18,5 +20,7 @@ def is_valid(num):
 
 
 if __name__ == "__main__":
-    ...
-
+   print(is_valid(500))
+   print(is_valid(50))
+   print(is_valid(0))
+   print(is_valid(-5))
--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 from guess_game.logic import player_input, is_valid
-from guess_game.messages import start_message
+from guess_game.messages import start_message, try_again
 from guess_game.functions import get_num, is_high, is_low ,is_num
 
+mynum = get_num()
+player_num = player_input()
 
-def turn():
-    get_num()
-    print(start_message)
-    while is_valid(player_input()):
-        is_high()
-        is_low()
-        is_num()
+def turn(my_num,player_num):
+    valid = is_valid(player_num)
+    while valid != try_again:
+        if is_high(mynum, valid):
+            print(is_high(mynum, valid))
+            return False
+        elif is_low(mynum, valid):
+            print(is_low(mynum, valid))
+            return False
+        elif is_num(mynum, valid):
+            print(is_num(mynum, valid))
+            return True
+        else:
+            return False
+
 
 
 
 
 
 if __name__ == "__main__":
-    turn()
+    print(start_message)
+
+    while True:
+        player_num
+        if turn(mynum,player_num):
+            continue
+
+
